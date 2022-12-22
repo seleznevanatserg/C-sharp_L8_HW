c9fe8a932a2ec6dc6a94200c1fd98e901bcdc090
SeleznevAS <seleznevanatserg@yahoo.com>
2022-12-22 21:04:15 +0300

    S8 and TGB delete

--- TGB/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Telegram.Bot;
-using Telegram.Bot.Types;
-
-TelegramBotClient client = new TelegramBotClient("5769280891:AAFZpEZXv10v3Np10AIYvk1AxWqtZojpMHs");
-
-User bot = client.GetMeAsync().Result;
-Console.WriteLine(bot.Username);
-
-Update[ ] updates = client.GetUpdatesAsync().Result;
